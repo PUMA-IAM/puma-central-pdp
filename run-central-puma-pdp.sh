13a30499b7f264ce7a1334fb35a4007b00384dd7
Maarten Decat <maarten.decat@gmail.com>
2014-04-02 16:52:54 +0200

    updated run script to be path-independent

--- run-central-puma-pdp.sh
+++ run-central-puma-pdp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -jar target/puma-central-puma-pdp.jar --policy-home /home/maartend/PhD/code/workspace-jee/puma-central-puma-pdp/resources/policies/
+java -jar target/puma-central-puma-pdp.jar --policy-home "$(pwd)/resources/policies/"
