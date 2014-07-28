b102a3bdfee995dd1d2ffe51b8887cf5c5b2df02
Jasper Bogaerts <jasper.bogaerts@cs.kuleuven.be>
2014-07-28 11:13:47 +0200

    Made the .sh-files to easily build/run the PDP more verbose

--- deploydnetcloud.sh
+++ deploydnetcloud.sh
@@ -2,14 +2,15 @@ echo "Building PIP utils"
 cd ../puma-pip-utils
 mvn -q clean install
 echo "Building PDP RMI utils"
-cd ../puma-central-puma-pdp-rmi-utils
+cd ../puma-central-pdp-rmi-utils
 mvn -q clean install
 echo "Building SunXACML...:"
 cd ../puma-sunxacml
 mvn -q clean install
 echo "Building central PDP..."
-cd ../puma-central-puma-pdp
+cd ../puma-central-pdp
 mvn -q clean install
+echo "Deploying..."
 cd target/
-scp puma-central-puma-pdp.jar ubuntu@dnetcloud-central:~
+scp puma-central-puma-pdp.jar ubuntu@puma-peglio:~
 cd ..
